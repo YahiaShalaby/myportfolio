1a433b8509feefe43a0ef3aa03d2991ab913892b
shalaby <yahia.shalaby@student.guc.edu.eg>
2024-02-21 14:39:21 +0200

    Skills+Projects+About

--- src/data.js
+++ src/data.js
@@ -5,7 +5,10 @@ import { Icon } from "@iconify/react";
 // import newLogo from "./images/yourFileName"
 
 // Projects Images (add your images to the images directory and import below)
-import Logo from "./images/logo.svg";
+// import Logo from "./images/logo.svg";
+import LSTM from "./images/5.png";
+import ConfMatrix from "./images/Confusion Matrix.png";
+import MostPopularBrands from "./images/Most Popular Brands.png";
 
 // Hero Images (add your images to the /images directory with the same names)
 import HeroLight from "./images/hero-light.jpg";
@@ -31,7 +34,7 @@ export const Blog = <Icon icon="ph:link-bold" />;
   Add a second paragraph for the about me section.
 */
 export const moreInfo =
-  "I am a Business Informatics senior at the GUC who is passionate about data analytics";
+  "As a Business Informatics senior at GUC, I thrive on unraveling insights from data through analytics, mining, and machine learning. My passion lies in applying these tools to uncover  hidden knowledge, combining theory with real-world applications in business or even foorball.";
 
 /* Skills
  ************************************************************** 
@@ -40,47 +43,47 @@ export const moreInfo =
 export const skillData = [
   {
     id: 1,
-    skill: <Icon icon="mdi:language-html5" className="display-4" />,
-    name: "HTML5",
+    skill: <Icon icon="simple-icons:python"  width="80" height="80"className="display-4" />,
+    name: "Python (Intermediate lvl)",
   },
   {
     id: 2,
-    skill: <Icon icon="ion:logo-css3" />,
-    name: "CSS3",
+    skill: <Icon icon="bxl:java" width="80" height="80" />,
+    name: "Java (Intermediate lvl)",
   },
   {
     id: 3,
-    skill: <Icon icon="fa6-brands:js"  />,
-    name: "JavaScript",
+    skill: <Icon icon="simple-icons:sap" width="80" height="80"/>,
+    name: "SAP ERP",
   },
   {
     id: 4,
-    skill: <Icon icon="ri:bootstrap-fill" />,
-    name: "BootStrap",
+    skill: <Icon icon="ph:file-sql" width="80" height="80" />,
+    name: "SQL",
   },
-  {
-    id: 5,
-    skill: <Icon icon="mdi:react" className="display-4" />,
-    name: "React",
-  },
-  {
-    id: 6,
-    skill: <Icon icon="file-icons:styledcomponents" className="display-4" />,
-    name: "Styled Components",
-  },
-  {
-    id: 7,
-    skill: <Icon icon="akar-icons:redux-fill" className="display-4" />,
-    name: "Redux",
-  },
-  {
-    id: 8,
-    skill: <Icon icon="bi:git" className="display-4" />,
-    name: "Git",
-  },
+  // {
+  //   id: 5,
+  //   skill: <Icon icon="mdi:react" className="display-4" />,
+  //   name: "React",
+  // },
+  // {
+  //   id: 6,
+  //   skill: <Icon icon="file-icons:styledcomponents" className="display-4" />,
+  //   name: "Styled Components",
+  // },
+  // {
+  //   id: 7,
+  //   skill: <Icon icon="akar-icons:redux-fill" className="display-4" />,
+  //   name: "Redux",
+  // },
+  // {
+  //   id: 8,
+  //   skill: <Icon icon="bi:git" className="display-4" />,
+  //   name: "Git",
+  // },
   {
     id: 9,
-    skill: <Icon icon="fa6-brands:square-github" className="display-4" />,
+    skill: <Icon icon="fa6-brands:square-github" width="96" height="96" className="display-4" />,
     name: "GitHub",
   },
 ];
@@ -93,13 +96,21 @@ export const resume = null;
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
 // export const filteredProjects = ["example-1", "example-2", "example-3"];
-export const filteredProjects = ["AI-LSTM-Project"];
+export const filteredProjects = ["AI-LSTM-Project","DM-Classification","DM-LinearReg"];
 
 // Replace the defualt GitHub image for matching repos below (images imported above - lines 7-8)
 export const projectCardImages = [
   {
-    name: "example-1",
-    image: Logo,
+    name: "AI-LSTM-Project",
+    image: LSTM,
+  },
+  {
+    name: "DM-Classification",
+    image: ConfMatrix,
+  },
+  {
+    name: "DM-LinearReg",
+    image: MostPopularBrands,
   },
 ];
 
